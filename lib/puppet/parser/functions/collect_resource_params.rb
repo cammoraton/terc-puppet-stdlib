96df1f01207719b2530c0641c32db12fe931ddc9
cammoraton <nick_cammorato@terc.edu>
2013-09-16 15:28:51 -0400

    Cleared description.

--- lib/puppet/parser/functions/collect_resource_params.rb
+++ lib/puppet/parser/functions/collect_resource_params.rb
@@ -4,18 +4,7 @@ require 'puppet/parser/functions'
 Puppet::Parser::Functions.newfunction(:collect_resource_params,
                                       :type => :rvalue,
                                       :doc => <<-'EOS'
-Takes a resource reference and an optional hash of attributes.
 
-Returns true if a resource with the specified attributes has already been added
-to the catalog, and false otherwise.
-
-    user { 'dan':
-      ensure => present,
-    }
-
-    if ! defined_with_params(User[dan], {'ensure' => 'present' }) {
-      user { 'dan': ensure => present, }
-    }
 EOS
 ) do |vals|
   
